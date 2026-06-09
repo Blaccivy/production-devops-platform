5f1ec32c6ba98f475b52ae52b8767ebadaf0be28
Shoyoye Dotun <shoyoyedotun032@gmail.com>
2026-06-09 10:52:35 +0100

    feat(terraform): add eks node iam role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,5 @@
+# IAM Role for EKS Cluster
+
 resource "aws_iam_role" "eks_cluster_role" {
   name = "${var.project_name}-eks-cluster-role"
 
@@ -13,4 +15,50 @@ resource "aws_iam_role" "eks_cluster_role" {
       }
     ]
   })
+}
+
+# Attach policies to EKS Cluster Role
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# IAM Role for EKS Nodes
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "${var.project_name}-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach policies to EKS Node Role
+
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+
+resource "aws_iam_role_policy_attachment" "ecr_read_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
